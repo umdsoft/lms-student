66eb2afbc36d3fe60d5edd10718827ef7ff27d9c
Umidbek <ujumaniyozov@ya.ru>
2025-11-01 10:38:23 +0500

    ASD

--- src/pages/users/UsersPage.vue
+++ src/pages/users/UsersPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="min-h-screen bg-gray-50">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+  <div>
+    <div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <!-- Header -->
       <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-6">
         <div>
